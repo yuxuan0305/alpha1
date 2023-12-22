b324c518f8c069c6ff6c22fa1295a7461b978fcd
Yu Yang Chen <yuyangchen@Yus-MacBook-Pro.local>
2023-12-21 22:01:17 -0800

    Styling of top navigation

--- src/components/topNav.tsx
+++ src/components/topNav.tsx
@@ -2,30 +2,43 @@ import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
 
 function TopNav() {
 
+    const appBarStyle = {
+        backgroundColor: 'black',
+        borderBottom: 'none',
+    };
+
+    const buttonStyle = {
+        color: 'inherit',
+        backgroundColor:'black',
+        fontFamily: 'Courier New, Courier, monospace',
+        padding: 25,
+    };
+
+    const centerToolbarStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
+
     return (
-      <AppBar position="static">
-        <Toolbar>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href="/"
-              sx={{
-                mr: 2,
-                fontWeight: 200,
-                fontFamily:'roboto',
-                color:'white',
-                letterSpacing: '.2rem',
-                textDecoration: 'none',
-              }}
-            >
-              Educative
-            </Typography>
+      <AppBar position='static' style={appBarStyle}>
+        <Toolbar style={centerToolbarStyle}>
+            <Button style={buttonStyle}>
+                Home
+            </Button>
+            <Button style={buttonStyle}>
+                Projects
+            </Button>
+            <Button style={buttonStyle}>
+                Experience
+            </Button>
         </Toolbar>
       </AppBar>
     );
-  }
+}
+
 export default TopNav;
